9c95d0340b3c88ef4ca3134673ee00f551c24fcf
Nesli <nesliparma@gmail.com>
2025-04-16 17:47:34 +0200

    function for finding unique authors

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
-'use strict';
+"use strict";
 
 const cakeRecipes = require("./cake-recipes.json");
 
-// Your functions here
+const getUniqueAuthors = (recipes) => {
+  const uniqueAuthors = [];
 
+  cakeRecipes.forEach((recipe) => {
+    const author = recipe.Author;
+    if (!uniqueAuthors.includes(author)) {
+      uniqueAuthors.push(author);
+    }
+  });
 
+  return uniqueAuthors.join("\n");
 
+  // FOR NUMBERED LIST
+  // const numberedList = uniqueAuthors
+  //   .map((author, index) => `${index + 1}. ${author}`)
+  //   .join("\n");
 
+  // return numberedList;
+};
 
-
-
-
-
-
-
-
+console.log(getUniqueAuthors(cakeRecipes));
 
 // Part 2
 
-const displayMenu = () => {
-  console.log("\nRecipe Management System Menu:");
-  console.log("1. Show All Authors");
-  console.log("2. Show Recipe names by Author");
-  console.log("3. Show Recipe names by Ingredient");
-  console.log("4. Get Recipe by Name");
-  console.log("5. Get All Ingredients of Saved Recipes");
-  console.log("0. Exit");
-  const choice = prompt("Enter a number (1-5) or 0 to exit: ");
-  return parseInt(choice);
-}
+// const displayMenu = () => {
+//   console.log("\nRecipe Management System Menu:");
+//   console.log("1. Show All Authors");
+//   console.log("2. Show Recipe names by Author");
+//   console.log("3. Show Recipe names by Ingredient");
+//   console.log("4. Get Recipe by Name");
+//   console.log("5. Get All Ingredients of Saved Recipes");
+//   console.log("0. Exit");
+//   const choice = prompt("Enter a number (1-5) or 0 to exit: ");
+//   return parseInt(choice);
+// };
 
+// let choice;
 
-let choice;
+// do {
+//   choice = displayMenu();
 
-do {
-  choice = displayMenu();
-
-  switch (choice) {
-    case 1:
-
-      break;
-    case 2:
-
-      break;
-    case 3:
-
-      break;
-    case 4:
-
-        break;
-    case 5:
-
-      break;
-    case 0:
-      console.log("Exiting...");
-      break;
-    default:
-      console.log("Invalid input. Please enter a number between 0 and 5.");
-  }
-} while (choice !== 0);
+//   switch (choice) {
+//     case 1:
+//       break;
+//     case 2:
+//       break;
+//     case 3:
+//       break;
+//     case 4:
+//       break;
+//     case 5:
+//       break;
+//     case 0:
+//       console.log("Exiting...");
+//       break;
+//     default:
+//       console.log("Invalid input. Please enter a number between 0 and 5.");
+//   }
+// } while (choice !== 0);
