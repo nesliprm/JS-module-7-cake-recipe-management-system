a97a2ace3987d31812ed5c98b216f42f42caf115
Nesli <nesliparma@gmail.com>
2025-04-17 13:10:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,10 +50,6 @@ const getRecipesByAuthor = (recipes, author) => {
   }
 };
 
-// NOTE FOR NEXT DAY
-// Log the names of these recipes to the console using the print function you just created.
-// YOURE DONE WITH THIS
-
 recipeNames(getRecipesByAuthor(cakeRecipes, "Gordon Ramsey Nilsen"));
 
 // Part 2
