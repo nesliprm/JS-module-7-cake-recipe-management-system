3676aeba2dd302f4b6631224953bf57197b4acca
Nesli <nesliparma@gmail.com>
2025-04-17 15:52:15 +0200

    finished part 1, installed prompt sync

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const cakeRecipes = require("./cake-recipes.json");
+const prompt = require("prompt-sync")();
 
 const getUniqueAuthors = (recipes) => {
   const uniqueAuthors = [];
@@ -59,42 +60,67 @@ const getRecipeByIngredient = (recipes, ingredient) => {
   return recipesListByIngredient;
 };
 
-recipeNames(getRecipeByIngredient(cakeRecipes, "140g caster sugar"));
+// recipeNames(getRecipeByIngredient(cakeRecipes, "140g caster sugar"));
+
+const getRecipeByName = (recipes, name) =>
+  recipes.find((recipe) => recipe.Name.includes(name));
+
+// console.log(getRecipeByName(cakeRecipes, "carrot"));
+
+const getIngredients = (recipes) => {
+  return recipes.reduce((ingredients, recipe) => {
+    return [...ingredients, ...recipe.Ingredients];
+  }, []);
+};
+
+// console.log(getIngredients(getRecipesByAuthor(cakeRecipes, "Angela Nilsen")));
 
 // Part 2
 
-// const displayMenu = () => {
-//   console.log("\nRecipe Management System Menu:");
-//   console.log("1. Show All Authors");
-//   console.log("2. Show Recipe names by Author");
-//   console.log("3. Show Recipe names by Ingredient");
-//   console.log("4. Get Recipe by Name");
-//   console.log("5. Get All Ingredients of Saved Recipes");
-//   console.log("0. Exit");
-//   const choice = prompt("Enter a number (1-5) or 0 to exit: ");
-//   return parseInt(choice);
-// };
+const displayMenu = () => {
+  console.log("\nRecipe Management System Menu:");
+  console.log("1. Show All Authors");
+  console.log("2. Show Recipe names by Author");
+  console.log("3. Show Recipe names by Ingredient");
+  console.log("4. Get Recipe by Name");
+  console.log("5. Get All Ingredients of Saved Recipes");
+  console.log("0. Exit");
+  const choice = prompt("Enter a number (1-5) or 0 to exit: ");
+  return parseInt(choice);
+};
 
-// let choice;
+let choice;
 
-// do {
-//   choice = displayMenu();
+do {
+  choice = displayMenu();
 
-//   switch (choice) {
-//     case 1:
-//       break;
-//     case 2:
-//       break;
-//     case 3:
-//       break;
-//     case 4:
-//       break;
-//     case 5:
-//       break;
-//     case 0:
-//       console.log("Exiting...");
-//       break;
-//     default:
-//       console.log("Invalid input. Please enter a number between 0 and 5.");
-//   }
-// } while (choice !== 0);
+  switch (choice) {
+    case 1:
+      console.log(getUniqueAuthors(cakeRecipes).join("\n"));
+      break;
+    case 2:
+      const authorInput = prompt("Enter an author name: ");
+      recipeNames(getRecipesByAuthor(cakeRecipes, authorInput));
+      break;
+    case 3:
+      const ingredientInput = prompt("Enter an ingredient: ");
+      recipeNames(getRecipeByIngredient(cakeRecipes, ingredientInput));
+      break;
+    case 4:
+      const recipeNameInput = prompt("Enter the name of a recipe: ");
+      console.log([getRecipeByName(cakeRecipes, recipeNameInput)]);
+      // if (recipe) {
+      //   recipeNames([recipe]);
+      // } else {
+      //   console.log("No recipe found with that name.");
+      // }
+      break;
+    case 5:
+      break;
+    case 0:
+      console.log("Exiting...");
+      break;
+    default:
+      console.log("Invalid input. Please enter a number between 0 and 5.");
+  }
+} while (choice !== 0);
